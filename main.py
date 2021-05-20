70aecb2e2ca20dde492d48949c41c80c5c3580e0
MaurusGubser <maurus.gubser@gmx.ch>
2021-05-20 18:23:19 +0200

    Defined updates for small boxes, rows and columns.

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-class Sudoku():
+class Sudoku:
     def __init__(self):
-        self.field_solution = np.zeros((9, 9))
-        self.field_possible = np.reshape([np.arange(1, 10) for i in range(0, 81)], shape=(9, 9, 9))
+        self.field_solution = np.zeros((9, 9), dtype=int)
+        self.field_possible = np.reshape([np.arange(1, 10, dtype=int) for i in range(0, 81)], (9, 9, 9))
 
     def set_number(self, x, y, number):
         self.field_solution[x, y] = number
@@ -19,23 +19,27 @@ class Sudoku():
         print(self.field_solution)
         return None
 
-    def check_small_box(self, box_x, box_y):
+    def show_possibilities(self):
+        print(self.field_possible)
+        return None
+
+    def update_small_box(self, box_x, box_y):
         changed = False
         temp = self.field_possible
         remove_numbers = set([])
-        for i in range(box_x, box_x + 3):
-            for j in range(box_y, box_y + 3):
+        for i in range(box_x*3, box_x*3 + 3):
+            for j in range(box_y*3, box_y*3 + 3):
                 remove_numbers.add(self.field_solution[i, j])
         remove_numbers.remove(0)
         for num in remove_numbers:
-            for i in range(box_x, box_x + 3):
-                for j in range(box_y, box_y + 3):
-                    self.field_possible[i, j, num] = 0
-        if abs(temp - self.field_possible) > 0:
+            for i in range(box_x*3, box_x*3 + 3):
+                for j in range(box_y*3, box_y*3+ 3):
+                    self.field_possible[i, j, int(num)] = 0
+        if np.any(abs(temp - self.field_possible)) > 0:
             changed = True
         return changed
 
-    def check_row(self, row_idx):
+    def update_row(self, row_idx):
         changed = False
         temp = self.field_possible
         remove_numbers = set([])
@@ -44,12 +48,12 @@ class Sudoku():
         remove_numbers.remove(0)
         for num in remove_numbers:
             for j in range(0, 9):
-                self.field_possible[row_idx, j, num] = 0
-        if abs(temp - self.field_possible) > 0:
+                self.field_possible[row_idx, j, int(num)] = 0
+        if np.any(abs(temp - self.field_possible)) > 0:
             changed = True
         return changed
 
-    def check_col(self, col_idx):
+    def update_col(self, col_idx):
         changed = False
         temp = self.field_possible
         remove_numbers = set([])
@@ -58,19 +62,28 @@ class Sudoku():
         remove_numbers.remove(0)
         for num in remove_numbers:
             for i in range(0, 9):
-                self.field_possible[col_idx, i, num] = 0
-        if abs(temp - self.field_possible) > 0:
+                self.field_possible[col_idx, i, int(num)] = 0
+        if np.any(abs(temp - self.field_possible)) > 0:
             changed = True
         return changed
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def check_for_solved(self):
+        if np.min(self.field_solution) == 0:
+            return False
+        else:
+            return True
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    sudoku_exple = Sudoku()
+    sudoku_exple.show_field()
+    sudoku_exple.set_number(8, 8, 1)
+    sudoku_exple.show_field()
+    sudoku_exple.show_possibilities()
+    sudoku_exple.update_small_box(2, 2)
+    sudoku_exple.update_row(8)
+    sudoku_exple.update_col(8)
+    print('Update')
+    sudoku_exple.show_possibilities()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
