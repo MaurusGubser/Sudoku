86114d4b9b85d935832410fb99d34f081e01e3d4
MaurusGubser <maurus.gubser@gmx.ch>
2021-10-10 21:01:35 +0200

    Introduced hough line transform and watershed algorithms.

--- ReadSudoku.py
+++ ReadSudoku.py
@@ -1,6 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib
 import cv2 as cv
+
+import matplotlib.pyplot as plt
+
+matplotlib.use('tkagg')
 
 
 class SudokuReader():
@@ -11,6 +15,7 @@ class SudokuReader():
         self.image_gray = np.empty((1, 1), dtype=np.uint8)
         self.edges = np.empty((1, 1), dtype=np.uint8)
         self.image_binary = np.empty((1, 1), dtype=np.uint8)
+        self.lines = np.empty((1, 1), dtype=np.uint8)
 
     def read_image_from_source(self, path_src):
         self.image = cv.imread(path_src)
@@ -21,53 +26,101 @@ class SudokuReader():
         return None
 
     def show_all_images(self):
-        plt.subplot(131)
-        plt.imshow(self.image)
-        plt.subplot(132)
-        plt.imshow(self.image_gray)
-        plt.subplot(133)
-        plt.imshow(self.image_binary)
-        #cv.waitKey(0)
+        fig, axs = plt.subplots(nrows=1, ncols=3)
+        axs[0].imshow(self.image)
+        axs[1].imshow(self.image_gray, cmap='gray')
+        axs[2].imshow(self.image_binary, cmap='gray')
         plt.show()
         return None
 
-    def show_images(self):
-        plt.subplot(121)
-        plt.imshow('Sudoku image gray', self.image_gray)
-        plt.subplot(122)
-        plt.imshow('Sudoku image edges', self.edges)
-        plt.show()
-        #cv.waitKey(0)
+    def show_all_images_opencv(self):
+        cv.imshow('Sudoku image', self.image)
+        cv.imshow('Sudoku image gray', self.image_gray)
+        cv.imshow('Sudoku image gray', self.image_binary)
+        cv.waitKey(0)
         return None
 
-    def save_image(self, path_dest):
+    def show_edge_image(self):
+        fig, axs = plt.subplots(nrows=1, ncols=2)
+        axs[0].imshow(self.image_gray, cmap='gray')
+        axs[1].imshow(self.edges, cmap='gray')
+        plt.show()
+        return None
+
+    def show_hough_line(self):
+        line_img = self.image.copy()
+        len_x, len_y, _ = self.image.shape
+        long_side = max(len_x, len_y)
+        for line in self.lines:
+            rho, theta = line[0]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + long_side * (-b))
+            y1 = int(y0 + long_side * (a))
+            x2 = int(x0 - long_side * (-b))
+            y2 = int(y0 - long_side * (a))
+            cv.line(line_img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        plt.imshow(line_img)
+        plt.show()
+
+    def save_images(self, path_dest):
         cv.imwrite(path_dest, self.image)
+        cv.imwrite(path_dest + '_gray', self.image_gray)
+        cv.imwrite(path_dest + '_binary', self.image_binary)
         return None
 
-    def compute_binary_image(self, thres):
-        self.image_binary = cv.adaptiveThreshold(self.image_gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 5, C=thres)
+    def compute_binary_image(self, thres=2.0, block_size=5):
+        self.image_binary = cv.adaptiveThreshold(self.image_gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,
+                                                 blockSize=block_size, C=thres)
         return None
 
-    def open_image(self, kernel_size):
-        kernel = cv.getStructuringElement(cv.MORPH_CROSS, (kernel_size, kernel_size))
+    def open_image(self, kernel_shape=cv.MORPH_CROSS, kernel_size=3):
+        kernel = cv.getStructuringElement(kernel_shape, (kernel_size, kernel_size))
         self.image_binary = cv.morphologyEx(self.image_binary, cv.MORPH_OPEN, kernel)
         return None
 
-    def otsu_thresholding(self, kernel_size):
+    def otsu_thresholding(self, kernel_size=3):
         blur = cv.GaussianBlur(self.image_gray, (kernel_size, kernel_size), 0)
         _, self.image_binary = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
         return None
 
-    def canny_edge_detection(self, thres_low, thres_upper):
+    def canny_edge_detection(self, thres_low=100, thres_upper=200):
         self.edges = cv.Canny(self.image_gray, thres_low, thres_upper)
         return None
 
-    def harris_corner(self):
+    def hough_line_detection(self, rho=1, theta=np.pi / 180, thres=200):
+        self.lines = cv.HoughLines(self.edges, rho=rho, theta=theta, threshold=thres)
+        return None
+
+    def watershed_detection(self, kernel_size=3, mask_size=5):
+        # noise removal
+        gray = cv.cvtColor(self.image, cv.COLOR_BGR2GRAY)
+        ret, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+        kernel = np.ones((kernel_size, kernel_size), np.uint8)
+        opening = cv.morphologyEx(thresh, op=cv.MORPH_OPEN, kernel=kernel, iterations=1)
+        # sure background area
+        sure_bg = cv.dilate(opening, kernel=kernel, iterations=1)
+        # Finding sure foreground area
+        dist_transform = cv.distanceTransform(opening, distanceType=cv.DIST_L2, maskSize=mask_size)
+        ret, sure_fg = cv.threshold(dist_transform, thresh=0.7 * dist_transform.max(), maxval=255, type=0)
+        # Finding unknown region
+        sure_fg = np.uint8(sure_fg)
+        unknown = cv.subtract(sure_bg, sure_fg)
+        fig, axs = plt.subplots(nrows=2, ncols=3)
+        axs[0, 0].imshow(self.image)
+        axs[0, 1].imshow(thresh)
+        axs[0, 2].imshow(sure_bg)
+        axs[1, 0].imshow(dist_transform)
+        axs[1, 1].imshow(sure_fg)
+        axs[1, 2].imshow(unknown)
+        plt.show()
+        return None
+
+    def harris_corner(self, block_size=2, ksize=3, k=0.15):
         self.image_gray = np.float32(self.image_gray)
-        blockSize = 2
-        ksize = 3
-        k = 0.15
-        dst = cv.cornerHarris(self.image_gray, blockSize, ksize, k)
+        dst = cv.cornerHarris(self.image_gray, blockSize=block_size, ksize=ksize, k=k)
         dst = cv.dilate(dst, None)
         self.image[dst > 0.01 * dst.max()] = [0, 0, 255]
         return None
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import cv2
+import numpy as np
+
 from Sudoku import Sudoku
 from ReadSudoku import SudokuReader
 
@@ -6,14 +9,16 @@ if __name__ == '__main__':
     reader = SudokuReader()
     path = 'Sudoku_Images/Sudoku4.jpg'
     reader.read_image_from_source(path)
-    #reader.show_image()
-    #reader.harris_corner()
-    reader.compute_binary_image(thres=2.3)
-    #reader.otsu_thresholding(kernel_size=3)
-    reader.show_all_images()
-    reader.open_image(3)
-    reader.show_all_images()
-
+    reader.compute_binary_image(thres=2.3, block_size=5)
+    reader.canny_edge_detection(thres_low=100, thres_upper=200)
+    reader.otsu_thresholding(kernel_size=3)
+    # reader.show_all_images()
+    # reader.show_edge_image()
+    # reader.open_image(kernel_shape=cv2.MORPH_CROSS, kernel_size=3)
+    # reader.hough_line_detection(rho=1.0, theta=np.pi/30, thres=120)
+    # reader.show_hough_line()
+    reader.watershed_detection()
+    # reader.harris_corner(block_size=2, ksize=3, k=0.15)
     """
     sudoku = Sudoku()
     sudoku.read_field_from_csv('Sudoku_Examples/Example6_Redundant')
