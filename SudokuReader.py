671388d4f2ed3ae5d8a2a9566e5100368bd46ce8
MaurusGubser <maurus.gubser@gmx.ch>
2021-10-25 16:19:17 +0200

    Added is_number_candidate for labelling connected comp.

--- SudokuReader.py
+++ SudokuReader.py
@@ -16,6 +16,8 @@ class SudokuReader():
         self.edges = np.empty((1, 1), dtype=np.uint8)
         self.image_binary = np.empty((1, 1), dtype=np.uint8)
         self.lines = np.empty((1, 1), dtype=np.uint8)
+        self.height = 0     # row
+        self.width = 0  # column
 
     def read_image_from_source(self, path_src):
         self.image = cv.imread(path_src)
@@ -23,6 +25,7 @@ class SudokuReader():
         scale_factor = 800 / long_side
         self.image = cv.resize(self.image, dsize=(0, 0), fx=scale_factor, fy=scale_factor)
         self.image_gray = cv.cvtColor(self.image, cv.COLOR_BGR2GRAY)
+        self.height, self.width = self.image_gray.shape
         return None
 
     def show_all_images(self):
@@ -71,19 +74,25 @@ class SudokuReader():
         cv.imwrite(path_dest + '_binary', self.image_binary)
         return None
 
-    def compute_binary_image(self, thres=2.0, block_size=5):
-        self.image_binary = cv.adaptiveThreshold(self.image_gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,
+    def compute_binary_image(self, gaussian_kernel_size=5, thres=1.0, block_size=5):
+        blur = cv.GaussianBlur(self.image_gray, (gaussian_kernel_size, gaussian_kernel_size), sigmaX=3.0)
+        self.image_binary = cv.adaptiveThreshold(blur, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV,
                                                  blockSize=block_size, C=thres)
         return None
 
-    def open_image(self, kernel_shape=cv.MORPH_CROSS, kernel_size=3):
+    def otsu_thresholding(self, kernel_size=7):
+        blur = cv.GaussianBlur(self.image_gray, (kernel_size, kernel_size), 0)
+        _, self.image_binary = cv.threshold(blur, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+        return None
+
+    def open_image(self, kernel_shape=cv.MORPH_RECT, kernel_size=3):
         kernel = cv.getStructuringElement(kernel_shape, (kernel_size, kernel_size))
         self.image_binary = cv.morphologyEx(self.image_binary, cv.MORPH_OPEN, kernel)
         return None
 
-    def otsu_thresholding(self, kernel_size=3):
-        blur = cv.GaussianBlur(self.image_gray, (kernel_size, kernel_size), 0)
-        _, self.image_binary = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    def close_image(self, kernel_shape=cv.MORPH_RECT, kernel_size=5):
+        kernel = cv.getStructuringElement(kernel_shape, (kernel_size, kernel_size))
+        self.image_binary = cv.morphologyEx(self.image_binary, cv.MORPH_CLOSE, kernel)
         return None
 
     def canny_edge_detection(self, thres_low=100, thres_upper=200):
@@ -94,7 +103,7 @@ class SudokuReader():
         self.lines = cv.HoughLines(self.edges, rho=rho, theta=theta, threshold=thres)
         return None
 
-    def watershed_detection(self, kernel_size=3, mask_size=5):
+    def watershed_detection(self, kernel_size=5, mask_size=5):
         # noise removal
         gray = cv.cvtColor(self.image, cv.COLOR_BGR2GRAY)
         ret, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
@@ -118,9 +127,61 @@ class SudokuReader():
         plt.show()
         return None
 
+    def label_connected_component(self):
+        self.otsu_thresholding()
+        self.close_image()
+        thresh = self.image_binary
+        numLabels, labels, stats, centroids = cv.connectedComponentsWithStats(thresh, connectivity=8, ltype=cv.CV_32S)
+        """
+        fig, axs = plt.subplots(nrows=1, ncols=2)
+        axs[0].imshow(thresh)
+        axs[1].imshow(labels)
+        plt.show()
+        """
+        for i in range(0, numLabels):
+            if i == 0:
+                continue
+            else:
+                text = "examining component {}/{}".format(i + 1, numLabels)
+
+                x = stats[i, cv.CC_STAT_LEFT]
+                y = stats[i, cv.CC_STAT_TOP]
+                w = stats[i, cv.CC_STAT_WIDTH]
+                h = stats[i, cv.CC_STAT_HEIGHT]
+                area = stats[i, cv.CC_STAT_AREA]
+
+                if self.is_number_candidate(stats[i]):
+                    print("[INFO] keeping component {}".format(text))
+                else:
+                    print("[INFO] refuse component {}".format(text))
+                (cX, cY) = centroids[i]
+                output = self.image.copy()
+                cv.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 3)
+                cv.circle(output, (int(cX), int(cY)), 4, (0, 0, 255), -1)
+                componentMask = (labels == i).astype("uint8") * 255
+                fig, axs = plt.subplots(nrows=1, ncols=2)
+                axs[0].imshow(output)
+                axs[1].imshow(componentMask)
+                plt.show()
+        return None
+
     def harris_corner(self, block_size=2, ksize=3, k=0.15):
         self.image_gray = np.float32(self.image_gray)
         dst = cv.cornerHarris(self.image_gray, blockSize=block_size, ksize=ksize, k=k)
         dst = cv.dilate(dst, None)
         self.image[dst > 0.01 * dst.max()] = [0, 0, 255]
         return None
+
+    def is_number_candidate(self, stats):
+        total_area = self.height * self.width
+        x = stats[cv.CC_STAT_LEFT]
+        y = stats[cv.CC_STAT_TOP]
+        w = stats[cv.CC_STAT_WIDTH]
+        h = stats[cv.CC_STAT_HEIGHT]
+        area_candidate = stats[cv.CC_STAT_AREA]
+        if h/w < 0.5 or 2.0 < h/w:
+            return False
+        elif area_candidate / total_area < 0.0001 or 0.003 < area_candidate / total_area:
+            return False
+        else:
+            return True
--- main.py
+++ main.py
@@ -7,17 +7,19 @@ from SudokuReader import SudokuReader
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     reader = SudokuReader()
-    path = 'Sudoku_Images/Sudoku4.jpg'
+    path = 'Sudoku_Images/Sudoku2.png'
     reader.read_image_from_source(path)
-    reader.compute_binary_image(thres=2.3, block_size=5)
-    reader.canny_edge_detection(thres_low=100, thres_upper=200)
-    reader.otsu_thresholding(kernel_size=3)
+
+    # reader.compute_binary_image(thres=2.3, block_size=5)
+    # reader.canny_edge_detection(thres_low=100, thres_upper=200)
+    # reader.otsu_thresholding(kernel_size=3)
     # reader.show_all_images()
     # reader.show_edge_image()
     # reader.open_image(kernel_shape=cv2.MORPH_CROSS, kernel_size=3)
     # reader.hough_line_detection(rho=1.0, theta=np.pi/30, thres=120)
     # reader.show_hough_line()
-    reader.watershed_detection()
+    # reader.watershed_detection()
+    reader.label_connected_component()
     # reader.harris_corner(block_size=2, ksize=3, k=0.15)
     """
     sudoku = Sudoku()
