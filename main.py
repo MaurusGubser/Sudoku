d3ed7d2bd00e34f76086ec1cb56a5bba4067b415
Maurus Gubser <maurus.gubser@originate.ch>
2022-02-28 22:20:21 +0100

    Call via read_and_solve_sudoku method.

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from SudokuSolver import SudokuSolver
 from SudokuReader import SudokuReader
 
-if __name__ == '__main__':
-    path_img = 'Sudoku_Images/Sudoku1.jpeg'
+
+def read_and_solve_sudoku(path_sudoku_img):
     path_clf = 'NumberClassifier_EuropeanDigits.h5'
-    reader = SudokuReader(path_img=path_img, path_clf=path_clf, debug=False)
+    reader = SudokuReader(path_img=path_sudoku_img, path_clf=path_clf, debug=False)
     if reader.get_sudoku_field_from_image():
         input_field = reader.sudoku_field
         solver = SudokuSolver()
         if solver.set_sudoku_field(input_field):
-            solver.solve_for_all_solutions()
-            print('Found {} solution(s)'.format(len(solver.solutions)))
-            for i in range(0, len(solver.solutions)):
-                reader.show_solution_on_sudoku(solver.solutions[i])
+            if solver.find_one_solution():
+                print('Found a solution.')
+                reader.save_solution_on_image(solver.field_sudoku, path_sudoku_img)
+            else:
+                raise RuntimeError('Did not find a solution')
+    return None
