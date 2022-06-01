1ddfb01e258d017faca3b74f70a05f05f7c4473e
MaurusGubser <maurus.gubser@gmx.ch>
2022-06-01 08:07:31 +0200

    Show solution, not drawing on image.

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ from SudokuReader import SudokuReader
 
 
 def read_and_solve_sudoku(path_sudoku_img):
-    path_clf = 'NumberClassifier_EuropeanDigits.h5'
-    reader = SudokuReader(path_img=path_sudoku_img, path_clf=path_clf, debug=False)
+    path_clf = 'model-OCR.h5'
+    reader = SudokuReader(path_img=path_sudoku_img, path_clf=path_clf, show_steps=True)
     if reader.get_sudoku_field_from_image():
         input_field = reader.sudoku_field
         solver = SudokuSolver()
         if solver.set_sudoku_field(input_field):
             if solver.find_one_solution():
                 print('Found a solution.')
-                reader.save_solution_on_image(solver.field_sudoku, path_sudoku_img)
+                # remove
+                reader.show_solution_on_sudoku(solver.solutions[0])
+                # reader.save_solution_on_image(solver.field_sudoku, path_sudoku_img)
             else:
                 raise RuntimeError('Did not find a solution')
     return None
+
+
+path = 'Sudoku_Images/Sudoku2.png'
+read_and_solve_sudoku(path)
