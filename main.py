7b2285c103c42ce02c96d24c6d4bfcd97f12ff85
Maurus Gubser <maurus.gubser@originate.ch>
2022-08-03 08:25:42 +0200

    tensorflow version 2.5.0

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from SudokuReader import SudokuReader
 
 def read_and_solve_sudoku(path_sudoku_img):
     path_clf = 'model-OCR.h5'
-    reader = SudokuReader(path_img=path_sudoku_img, path_clf=path_clf, show_steps=True)
+    reader = SudokuReader(path_img=path_sudoku_img, path_clf=path_clf, show_steps=False)
     if reader.get_sudoku_field_from_image():
         input_field = reader.sudoku_field
         solver = SudokuSolver()
@@ -17,5 +17,6 @@ def read_and_solve_sudoku(path_sudoku_img):
                 raise RuntimeError('Did not find a solution')
     return None
 
-path = 'Sudoku_Images/Test3.jpg'
+
+path = 'Sudoku_Images/Test1.jpg'
 read_and_solve_sudoku(path)
